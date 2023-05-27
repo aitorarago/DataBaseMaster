b433f86f972fcae338f9015ea38e7674b1218ee7
aitorarago <aitorarago@elpuig.xeill.net>
2023-05-27 18:23:09 +0200

    finish

--- src/main/resources/com/example/projectofinal/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.projectofinal","l":"com.example.projectofinal"},{"m":"com.example.projectofinal","l":"com.example.projectofinal.Clases.Funciones"},{"m":"com.example.projectofinal","l":"com.example.projectofinal.Clases.Personalizacion"}];updateSearchResults();
